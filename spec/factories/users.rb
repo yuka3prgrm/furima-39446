1650ac74219ddcded755d945842469bfabb9929b
yuka3prgrm <yuka3prgrm@gmail.com>
2023-06-14 00:58:59 +0900

    ユーザー管理機能（修正）

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     end
     nickname {Faker::Name.last_name}
     email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length: 6)}
+    password {'1a' + Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     first_name {person.first.kanji }
     last_name {person.last.kanji }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -78,6 +78,12 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password include both letters and numbers')
       end
+      it 'passwordが全角は登録できない' do
+        @user.password = 'abcde１'
+        @user.password_confirmation = 'abcde１'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password include both letters and numbers')
+      end
       it 'passwordとpassword_confirmationが不一致では登録できない' do
         @user.password = '123456'
         @user.password_confirmation = '1234567'
@@ -94,6 +100,26 @@ RSpec.describe User, type: :model do
         @user.email = 'testmail'
         @user.valid?
         expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+      it 'first_nameに半角文字が含まれている場合は登録できない' do
+        @user.first_name = 'ﾐｮｳｼﾞ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('First name is invalid. Input full-width characters.')
+      end
+      it 'last_nameに半角文字が含まれている場合は登録できない' do
+        @user.last_name = 'ﾅﾏｴ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Last name is invalid. Input full-width characters.')
+      end
+      it 'first_name_kanaにカナ以外の文字（ひらがなや漢字）が含まれている場合は登録できない' do
+        @user.first_name_kana = '苗字'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('First name kana is invalid. Input full-width katakana characters.')
+      end
+      it 'last_name_kanaにカナ以外の文字（ひらがなや漢字）が含まれている場合は登録できない' do
+        @user.last_name_kana = '名前'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Last name kana is invalid. Input full-width katakana characters.')
       end
     end
   end
