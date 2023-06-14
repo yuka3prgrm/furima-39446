7324698e1cf86f646a0288a1d7a659f274d97572
yuka3prgrm <yuka3prgrm@gmail.com>
2023-06-14 14:15:23 +0900

    ユーザー管理機能（修正２）

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,22 +32,22 @@ RSpec.describe User, type: :model do
       it "first_nameが空では登録できない" do
         @user.first_name = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name can't be blank", "First name is invalid. Input full-width characters.")
+        expect(@user.errors.full_messages).to include("First name can't be blank")
       end
       it "last_nameが空では登録できない" do
         @user.last_name = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name can't be blank","Last name is invalid. Input full-width characters.")
+        expect(@user.errors.full_messages).to include("Last name can't be blank")
       end
       it "first_name_kanaが空では登録できない" do
         @user.first_name_kana = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name kana can't be blank", "First name kana is invalid. Input full-width katakana characters.")
+        expect(@user.errors.full_messages).to include("First name kana can't be blank")
       end
       it "last_name_kanaが空では登録できない" do
         @user.last_name_kana = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name kana can't be blank","Last name kana is invalid. Input full-width katakana characters.")
+        expect(@user.errors.full_messages).to include("Last name kana can't be blank")
       end
       it "birthdayが空では登録できない" do
         @user.birthday = ''
