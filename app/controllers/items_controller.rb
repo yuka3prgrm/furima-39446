0eb3dd8bbcbb20da925f4e1c013ef031361ec7d6
yuka3prgrm <yuka3prgrm@gmail.com>
2023-06-14 16:46:13 +0900

    出品された商品の保存機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order("created_at DESC")
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :explanation, :explanation, :category_id, :situation_id, :postage_id, :prefecture_id, :shipment_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,15 @@ class Item < ApplicationRecord
   
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :situation
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :shipment
 
   with_options presence: true do
     validates :name
     validates :explanation
-    validates :price
+    validates :price, format: { with: /\A[3-9]\d{2}|[1-9]\d{3,6}|9999999\z/ }
     validates :image
   end
   with_options numericality: { other_than: 1, message: "can't be blank"} do
