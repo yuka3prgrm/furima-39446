e8d7199589964cf3f1f9a6cf2998ebb811c411ef
yuka3prgrm <yuka3prgrm@gmail.com>
2023-06-15 21:33:18 +0900

    編集ページの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_prototypes, only: [:edit, :show]
+  before_action :move_to_index, only: :edit
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -18,13 +20,35 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
+  def edit
+  end
+  
+  def update
+    @item = Item.find(params[:id])
+      if @item.update(item_params)
+        redirect_to item_path(@item)
+      else
+        render :edit
+      end
+  end
+  
   private
 
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :situation_id, :postage_id, :prefecture_id,
                                  :shipment_id, :price, :image).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
+
+  def set_prototypes
+    @item = Item.find(params[:id])
+  end
+
 end
