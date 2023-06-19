eb11f05bc28b4dd831d97e4fa25a53a3bbf06942
yuka3prgrm <yuka3prgrm@gmail.com>
2023-06-20 01:25:00 +0900

    sold out表示の有無

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,7 @@ class ItemsController < ApplicationController
   before_action :set_prototypes, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :destroy]
   def index
+ 
     @items = Item.all.order("created_at DESC")
   end
 
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,9 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :move_to_index, only: [:index, :create]
+
   def index
     @order_address = OrderAddress.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
@@ -22,8 +24,14 @@ class OrdersController < ApplicationController
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
-  def pay_item
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    if @item.order.presence || current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
 
+  def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  
     Payjp::Charge.create(
       amount: @item.price,  
