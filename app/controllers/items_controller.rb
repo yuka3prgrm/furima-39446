e078b4f51c1b9cfdf6241ae7e204c4b70f566afb
yuka3prgrm <yuka3prgrm@gmail.com>
2023-06-20 16:06:28 +0900

    売却済み商品の編集ページに遷移できないようにする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,7 +25,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    binding.pry
   end
   
   def update
@@ -49,7 +48,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? && current_user.id == @item.user_id
+    if @item.order.presence || !(user_signed_in? && current_user.id == @item.user_id) 
       redirect_to action: :index
     end
   end
