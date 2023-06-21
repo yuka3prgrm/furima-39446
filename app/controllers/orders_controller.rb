1131033e49af046bba46506fd164c6e7f3196581
yuka3prgrm <yuka3prgrm@gmail.com>
2023-06-21 09:46:04 +0900

    購入機能（修正3）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -24,6 +24,7 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
+    @item = Item.find(params[:item_id])
     if @item.order.presence || current_user.id == @item.user_id
       redirect_to root_path
     end
